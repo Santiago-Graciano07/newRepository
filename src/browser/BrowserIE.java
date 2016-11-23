0bc1af4101a2548a3cc11f82cbf59a41cb43a894
Talos Digital <talosdigital@Taloss-MacBook-Pro.local>
2016-11-23 16:02:35 -0500

    browser IE

---
+++ src/browser/BrowserIE.java
@@ -0,0 +1,14 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class BrowserIE {
+	static WebDriver driver;
+	public static void main(String[] args) {
+		driver = new InternetExplorerDriver(); 
+
+		//this is class of internet explorer
+	}
+
+}
