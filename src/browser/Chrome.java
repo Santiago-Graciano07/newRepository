94385c61c7405908fb9c21f220365d6dbcc38b61
Talos Digital <talosdigital@Taloss-MacBook-Pro.local>
2016-11-23 15:20:17 -0500

    All

---
+++ src/browser/Chrome.java
@@ -0,0 +1,18 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "/Users/talosdigital/Downloads/chromedriver 2");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+	
+	}
+
+}
---
+++ src/browser/Safari.java
@@ -0,0 +1,21 @@
+package browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Safari {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new SafariDriver();
+		driver.get("http://store.demoqa.com");
+ 
+		//Find some element on DemoQa.com
+		WebElement element = driver.findElement(By.id("login"));
+		element.click();
+	}
+
+}
